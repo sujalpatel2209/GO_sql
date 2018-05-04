fc3e602d4033f0e714d3981000391f34b7988014
Sujal Patel <php2.webmob@gmail.com>
2018-05-04 09:51:53 +0530

    change

--- dbConnect.go
+++ dbConnect.go
@@ -16,14 +16,11 @@ type User struct {
 func main() {
 	db := dbConn()
 	selDb, err := db.Query("SELECT * FROM user")
-
-	emp := User{}
+	user := User{}
 	res := []User{}
-
 	if err != nil{
 		panic(err.Error())
 	}
-
 	for selDb.Next() {
 		var id, age int
 		var firstname, lastname string
@@ -31,28 +28,21 @@ func main() {
 		if err != nil{
 			panic(err.Error())
 		}
-		emp.id = id
-		emp.firstname = firstname
-		emp.lastname = lastname
-		emp.age = age
-		res = append(res, emp)
+		user.id = id
+		user.firstname = firstname
+		user.lastname = lastname
+		user.age = age
+		res = append(res, user)
 	}
-
 	fmt.Println("Data : ", res)
 	defer db.Close()
-
-
 }
 
 func dbConn() (db *sql.DB){
-
 	db, err := sql.Open("mysql","root:@/gocrud")
-
 	if err != nil{
 		panic(err.Error())
 	}
-
 	return db
-
 }
 
